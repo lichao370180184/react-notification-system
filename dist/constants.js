d6a9c73d028bdbe9f5370f3cea01330baae45d8e
Iñaki Baz Castillo <ibc@aliax.net>
2016-09-10 13:28:20 +0200

    Add dist/ to the repo si it can be properly installed via `npm install`

---
+++ dist/constants.js
@@ -0,0 +1,35 @@
+var CONSTANTS = {
+
+  // Positions
+  positions: {
+    tl: 'tl',
+    tr: 'tr',
+    tc: 'tc',
+    bl: 'bl',
+    br: 'br',
+    bc: 'bc'
+  },
+
+  // Levels
+  levels: {
+    success: 'success',
+    error: 'error',
+    warning: 'warning',
+    info: 'info'
+  },
+
+  // Notification defaults
+  notification: {
+    title: null,
+    message: null,
+    level: null,
+    position: 'tr',
+    autoDismiss: 5,
+    dismissible: true,
+    action: null,
+    image: null
+  }
+};
+
+
+module.exports = CONSTANTS;
